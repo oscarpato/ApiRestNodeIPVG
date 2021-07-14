a9a9150f2ca2e667e25bb9a9b6538817dbb76af5
OscarSalgado <oscar.salgado@genesys.cl>
2021-07-14 15:21:00 -0400

    Update1 desafio.js

--- desafio1.js
+++ desafio1.js
@@ -1,5 +1,25 @@
-let frase="prueba una. frase";
-//contar cuantas veces existe una palabra en la frase.
-frase.replace(".","");
+//Desafío1: contar cuantas veces existe una palabra en la frase, sin considerar espacios ni otros caracteres.
 
-console.log(frase.replace(".",""));
+let frase="  Prueba   45 una.   fRase .  _'";
+let fraseLimpia;
+
+//let variosEspacios = /\s+(?=\s)/g;
+let soloLetras = /[^a-zA-Z]/g;
+let variosEspacios = /\s+/g;
+let espacioInicio = /^ /g;
+let espacioFin = / $/g;
+
+fraselimpia = frase.replace(soloLetras," ");
+fraseLimpia = fraselimpia.replace(variosEspacios, " ");
+fraseLimpia = fraseLimpia.replace(espacioInicio,"");
+fraseLimpia = fraseLimpia.replace(espacioFin,"");
+
+let fraseDividida = fraseLimpia.split(" ");
+let totalPalabras = fraseDividida.length;
+
+console.log(frase);
+console.log(fraseLimpia);
+console.log(totalPalabras);
+console.log(fraseDividida);
+
+//recorrer con for
